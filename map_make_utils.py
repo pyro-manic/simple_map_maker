5d6c5e0543326c1ac34a827f6da9828f01a9c476
moses297 <moshe.st297@gmail.com>
2016-11-10 10:46:33 +0200

    Add files via upload

---
+++ map_make_utils.py
@@ -0,0 +1,18 @@
+class RoomData(object):
+    items = ["Book", "Sword", "Fireplace", "Door"]
+    floor = ["Stone", "Poo", "Pee", "Blood"]
+
+
+class RoomGenerator(object):
+    def __init__(self, room_width, room_height):
+        self.room_width = room_width
+        self.room_height = room_height
+        self.room_type = None
+        self.room_board = [self.room_width][self.room_height]
+
+    def create_room_(self):
+        pass
+
+    def randomize_room_objects(self):
+        for obj in RoomData.items:
+            print obj
